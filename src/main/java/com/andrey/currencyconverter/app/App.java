9837503a90a866a34a18640ee86f656d215d852f
andreyDelay <frowzygleb@gmail.com>
2022-04-28 17:57:52 +0300

    spring boot came into the picture

--- src/main/java/com/andrey/currencyconverter/app/App.java
+++ src/main/java/com/andrey/currencyconverter/app/App.java
@@ -1,18 +1,16 @@
 package com.andrey.currencyconverter.app;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
-@Configuration
-@PropertySource("application.properties")
+@SpringBootApplication
 @ComponentScan("com.andrey.currencyconverter")
 public class App
 {
     public static void main( String[] args ) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
+        ConfigurableApplicationContext context = SpringApplication.run(App.class, args);
         ApplicationFlow applicationFlow = context.getBean(ApplicationFlow.class);
         applicationFlow.startFlow();
     }
